4870ba13e51a7dd89741f6d7a50bf4823ab28689
dipalilule <87165955+dipalilule@users.noreply.github.com>
2021-07-10 19:09:32 +0530

    githubserver.js

--- githubserver.js
+++ githubserver.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // app
 const app = exress ();
 app.get('/', (req, res) = {
-  res.send(h1 style"color:red:">java home = version 3- running on ecs!!!
+  res.send(h1 style"color:blue:">java home = version 4- running on ecs!!!
 }};
         
  app. listen(port, host);
